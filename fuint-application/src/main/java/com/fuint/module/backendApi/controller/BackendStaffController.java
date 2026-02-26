836414faea04bfee527009110d5c4140b84871e2
fushengqian <fushengqian@qq.com>
2026-02-26 14:28:43 +0800

    fixed 员工管理相关接口优化迭代

--- fuint-application/src/main/java/com/fuint/module/backendApi/controller/BackendStaffController.java
+++ fuint-application/src/main/java/com/fuint/module/backendApi/controller/BackendStaffController.java
@@ -1,18 +1,17 @@
 package com.fuint.module.backendApi.controller;
 
-import com.fuint.common.Constants;
 import com.fuint.common.dto.AccountInfo;
 import com.fuint.common.dto.ParamDto;
 import com.fuint.common.dto.StaffDto;
 import com.fuint.common.enums.StaffCategoryEnum;
 import com.fuint.common.enums.StatusEnum;
+import com.fuint.common.param.StaffPage;
 import com.fuint.common.param.StaffParam;
 import com.fuint.common.service.StaffService;
 import com.fuint.common.util.CommonUtil;
 import com.fuint.common.util.PhoneFormatCheckUtils;
 import com.fuint.common.util.TokenUtil;
 import com.fuint.framework.exception.BusinessCheckException;
-import com.fuint.framework.pagination.PaginationRequest;
 import com.fuint.framework.pagination.PaginationResponse;
 import com.fuint.framework.web.BaseController;
 import com.fuint.framework.web.ResponseObject;
@@ -24,7 +23,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,40 +51,15 @@ public class BackendStaffController extends BaseController {
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     @CrossOrigin
     @PreAuthorize("@pms.hasPermission('staff:list')")
-    public ResponseObject list(HttpServletRequest request) throws BusinessCheckException {
-        Integer page = request.getParameter("page") == null ? Constants.PAGE_NUMBER : Integer.parseInt(request.getParameter("page"));
-        Integer pageSize = request.getParameter("pageSize") == null ? Constants.PAGE_SIZE : Integer.parseInt(request.getParameter("pageSize"));
-        String mobile = request.getParameter("mobile");
-        String realName = request.getParameter("realName");
-        String auditedStatus = request.getParameter("auditedStatus");
-        String storeId = request.getParameter("storeId");
-        String category = request.getParameter("category");
-
+    public ResponseObject list(@ModelAttribute StaffPage staffPage) throws BusinessCheckException {
         AccountInfo accountInfo = TokenUtil.getAccountInfo();
         if (accountInfo.getStoreId() != null && accountInfo.getStoreId() > 0) {
-            storeId = accountInfo.getStoreId().toString();
+            staffPage.setStoreId(accountInfo.getStoreId());
         }
-
-        Map<String, Object> params = new HashMap<>();
         if (accountInfo.getMerchantId() != null && accountInfo.getMerchantId() > 0) {
-            params.put("merchantId", accountInfo.getMerchantId());
+            staffPage.setMerchantId(accountInfo.getMerchantId());
         }
-        if (StringUtil.isNotEmpty(realName)) {
-            params.put("name", realName);
-        }
-        if (StringUtil.isNotEmpty(mobile)) {
-            params.put("mobile", mobile);
-        }
-        if (StringUtil.isNotEmpty(auditedStatus)) {
-            params.put("status", auditedStatus);
-        }
-        if (StringUtil.isNotEmpty(storeId)) {
-            params.put("storeId", storeId);
-        }
-        if (StringUtil.isNotEmpty(category)) {
-            params.put("category", category);
-        }
-        PaginationResponse<StaffDto> paginationResponse = staffService.queryStaffListByPagination(new PaginationRequest(page, pageSize, params));
+        PaginationResponse<StaffDto> paginationResponse = staffService.queryStaffListByPagination(staffPage);
 
         // 员工类别列表
         List<ParamDto> categoryList = StaffCategoryEnum.getStaffCategoryList();
